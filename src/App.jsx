007f6716a951e9659eb5e0ad4eaab1b52daa5c9c
Charles Lamb <charles.lamb.dev@gmail.com>
2026-08-22 06:39:55 -0400

    updated app.jsx with new routes

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,12 @@ import LeadDetail from './pages/admin/LeadDetail'
 import LogCall from './pages/admin/LogCall'
 import ConvertLead from './pages/admin/ConvertLead'
 import NewLead from './pages/admin/NewLead'
+import RequestsInbox from './pages/admin/RequestsInbox'
+import ClientDetail from './pages/admin/ClientDetail'
+import ProjectEditor from './pages/admin/ProjectEditor'
 
 import Overview from './pages/portal/Overview'
+import NewRequest from './pages/portal/NewRequest'
 import Project from './pages/portal/Project'
 import Requests from './pages/portal/Requests'
 import RequestDetail from './pages/portal/RequestDetail'
@@ -49,7 +53,7 @@ export default function App() {
             <Route index element={<Overview />} />
             <Route path="project" element={<Project />} />
             <Route path="requests" element={<Requests />} />
-            <Route path="requests/new" element={<Placeholder title="New request" />} />
+            <Route path="requests/new" element={<NewRequest />} />
             <Route path="requests/:id" element={<RequestDetail />} />
             <Route path="files" element={<Placeholder title="Files" />} />
             <Route path="billing" element={<Placeholder title="Billing" />} />
@@ -61,17 +65,17 @@ export default function App() {
           {/* admin console */}
           <Route path="/admin" element={<ProtectedRoute staff><Shell /></ProtectedRoute>}>
             <Route index element={<Today />} />
-            <Route path="requests" element={<Placeholder title="Requests inbox" />} />
-            <Route path="requests/:id" element={<Placeholder title="Request" />} />
+            <Route path="requests" element={<RequestsInbox />} />
+            <Route path="requests/:id" element={<RequestsInbox />} />
             <Route path="pipeline" element={<Pipeline />} />
             <Route path="leads/new" element={<NewLead />} />
             <Route path="leads/:id" element={<LeadDetail />} />
             <Route path="leads/:id/log" element={<LogCall />} />
             <Route path="leads/:id/convert" element={<ConvertLead />} />
             <Route path="clients" element={<Placeholder title="Clients" />} />
-            <Route path="clients/:id" element={<Placeholder title="Client" />} />
+            <Route path="clients/:id" element={<ClientDetail />} />
             <Route path="projects" element={<Placeholder title="Projects" />} />
-            <Route path="projects/:id" element={<Placeholder title="Project editor" />} />
+            <Route path="projects/:id" element={<ProjectEditor />} />
             <Route path="invoices" element={<ProtectedRoute owner><Placeholder title="Invoices" /></ProtectedRoute>} />
             <Route path="agents" element={<ProtectedRoute owner><Placeholder title="Agents" /></ProtectedRoute>} />
           </Route>
